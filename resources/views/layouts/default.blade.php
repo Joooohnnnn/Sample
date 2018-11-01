519a2b0ead30f39a8d141ea099d7f23fb596dcef
Joooohnnnn <376870483@qq.com>
2018-11-01 03:17:52 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Sample App') - L·Weibo</title>
     <link rel="stylesheet" href="/css/app.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    @include('layouts._header')
-
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            @include('shared._messages')
-            @yield('content')
-            @include('layouts._footer')
-        </div>
-
+@include('layouts._header')
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
+</div>
+<script src="/js/app.js"></script>
 </body>
 </html>
